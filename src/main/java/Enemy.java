cace00dbb7623748d17cac07a1279bb41efecdc4
Leticia <leticia-olive@hotmail.com>
2025-03-15 17:09:42 -0300

    Criando a base

---
+++ src/main/java/Enemy.java
@@ -0,0 +1,19 @@
+public class Enemy {
+    private String name;
+    private int health;
+    private int attackPower;
+    private double defense;
+    private boolean isBoss;
+    private String type;
+    private int experiencePoints;
+    private double armor;
+    private double speed;
+    private double accuracy;
+    private double range;
+    private double attackSpeed;
+    private double damageType;
+    private double attackRange;
+    private double attackDelay;
+    private double attackDamage;
+
+}
---
+++ src/main/java/Player.java
@@ -0,0 +1,112 @@
+
+public class Player {
+    private String name;
+    private int health;
+    private int attackPower;
+    private int defense;
+    private int experiencePoints;
+    private int level;
+    private int gold;
+    private int currentWeapon;
+    private int currentArmor;
+    private int currentHealthPotion;
+    private int currentManaPotion;
+    private int currentStrengthPotion;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getAttackPower() {
+        return attackPower;
+    }
+
+    public void setAttackPower(int attackPower) {
+        this.attackPower = attackPower;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getExperiencePoints() {
+        return experiencePoints;
+    }
+
+    public void setExperiencePoints(int experiencePoints) {
+        this.experiencePoints = experiencePoints;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    public int getCurrentWeapon() {
+        return currentWeapon;
+    }
+
+    public void setCurrentWeapon(int currentWeapon) {
+        this.currentWeapon = currentWeapon;
+    }
+
+    public int getCurrentArmor() {
+        return currentArmor;
+    }
+
+    public void setCurrentArmor(int currentArmor) {
+        this.currentArmor = currentArmor;
+    }
+
+    public int getCurrentHealthPotion() {
+        return currentHealthPotion;
+    }
+
+    public void setCurrentHealthPotion(int currentHealthPotion) {
+        this.currentHealthPotion = currentHealthPotion;
+    }
+
+    public int getCurrentManaPotion() {
+        return currentManaPotion;
+    }
+
+    public void setCurrentManaPotion(int currentManaPotion) {
+        this.currentManaPotion = currentManaPotion;
+    }
+
+    public int getCurrentStrengthPotion() {
+        return currentStrengthPotion;
+    }
+
+    public void setCurrentStrengthPotion(int currentStrengthPotion) {
+        this.currentStrengthPotion = currentStrengthPotion;
+    }
+}
